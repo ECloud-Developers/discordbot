73bec1e2fa6d6933a4d977583130868ce0a2d4a6
rinzler100 <39069176+rinzler100@users.noreply.github.com>
2020-05-01 14:07:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const client = new Discord.Client();
 // 'client.on('message')' commands are triggered when the
 // specified message is read in a text channel that the bot is in.
 
+client.login("NzAzNzIwOTY1NjAyODAzODA0.XqxgKA.dartWketRiHnqjhcOssRGoJ9Ov8");
+
 client.on('message', message => {
   if (message.content === 'ping') {
     message.reply('pong');
   }
 });
 
-client.login("NzAzNzIwOTY1NjAyODAzODA0.XqxgKA.dartWketRiHnqjhcOssRGoJ9Ov8");
+client.on('message', message => {
+  if (message.content === 'soundcloud') {
+    message.reply('Go check out the best soundcloud profile here: https://soundcloud.com/electro_cloud');
+  }
+});
 
 
